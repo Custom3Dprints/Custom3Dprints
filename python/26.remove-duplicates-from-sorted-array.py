9f94ae3d9bded07e9cee6ee45650000864c272bc
Marjorie Middlesteadt <marjorie.3dprints@gmail.com>
2023-11-26 17:02:28 -0500

    Pictures update

--- python/26.remove-duplicates-from-sorted-array.py
+++ python/26.remove-duplicates-from-sorted-array.py
@@ -7,10 +7,18 @@
 # @lc code=start
 class Solution(object):
     def removeDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        nums[:] = (sorted(set(nums)))
+        print(nums)
         
+        dups = []
+        for i in nums:
+            if i not in dups:
+                dups.append(i)
+
+        print(nums)
+        #return (dups)
+        
+
+Solution().removeDuplicates([1,1,2])
 # @lc code=end
 
---
+++ python/27.remove-element.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=27 lang=python
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution(object):
+    def removeElement(self, nums, val):
+        
+        
+# @lc code=end
+
