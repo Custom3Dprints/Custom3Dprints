3fa3973c3a852d8a06249eb8f55b3595771936ec
Marjorie Middlesteadt <marjorie.3dprints@gmail.com>
2024-01-09 22:52:37 -0500

    //

---
+++ JS_course/3D site/3Djs.js
@@ -0,0 +1,29 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo')
+
+//Mobile Menu
+menu.addEventListener('click', function(){
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+});
+
+
+function Home() {
+    window.location.href = "3Dhtml.html";
+}
+function Pictures() {
+    window.location.href = "3DPictures.html";
+}
+function Links() {
+    window.location.href = "3DLinks.html";
+}
+function Specials() {
+    window.location.href = "Valentine's.html";
+}
+function About() {
+    window.location.href = "3DAbout.html";
+}
+function Order() {
+    window.location.href = "3DOrder.html";
+}
