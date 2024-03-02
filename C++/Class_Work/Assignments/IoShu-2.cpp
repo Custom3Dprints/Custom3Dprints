cc9790cf0fe9f82fc1cb947bec421d0bf8342888
Marjorie <marjoriemiddle@gmail.com>
2024-03-02 09:40:15 -0800

    IoShu semi done

--- C++/Class_Work/Assignments/IoShu-2.cpp
+++ C++/Class_Work/Assignments/IoShu-2.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 const int rows = 7;
 const int cols = 7;
 
 // Function to print the 2D array
-void printArray(int arr[][cols]) {
+void printArray(vector<vector<string> >& arr) {
     for(int i = 0; i < rows; ++i) {
         for(int j = 0; j < cols; ++j) {
             cout << arr[i][j] << " ";
@@ -14,32 +17,84 @@ void printArray(int arr[][cols]) {
     }
 }
 
-void formatting(int arr[][cols]){
+
+void formatting(vector<vector<string> >& arr){
     for(int i = 0; i < rows; ++i) {
         for(int j = 0; j < cols; ++j) {
-            if (i %2!=0){
-                arr[i][j] = '-';
+            if (i %2==0){
+                arr[i][j] = "-";
+            }else{
+                if (j %2 == 0){
+                    arr[i][j] ="|";
+                }
             }
-            
         }
     }
 }
+int getinput(vector<vector<string> >& arr){
+    vector <int>inputs;
+    int number;
+    cout << "Enter Nine Numbers (1-9)\n";
+    for(int n = 1; n < 10; n++){
+        cout << "\tNumber " << n << ": ";
+        cin >> number;
 
-int Board() { //main
-    int arr[rows][cols];
+        while (number < 1 || number > 9){
+            cout << "\tNumber " << n << ": ";
+            cin >> number;
+        }
+        while (find(inputs.begin(), inputs.end(), number) != inputs.end()){
+            cout << "\tNumber " << n << ": ";
+            cin >> number;
+        }
+        inputs.push_back(number);
+    }
 
-    // Initialize elements of the array
+    int count = 0;
+    for (int i = 0; i < rows; i++){
+        for (int j =0; j < cols; j++){
+            if (arr[i][j] != "-" && arr[i][j] != "|"){
+                arr[i][j] = to_string(inputs[count]);
+                count++;
+            }
+        }
+    }
+   return 0;
+}
+/*
+void update(string arr[rows][cols]){
+    int count = 0;
+    for (int i = 0; i < rows; i++){
+        for (int j =0; j < cols; j++){
+            if (arr[rows][cols] != "-" && arr[rows][cols] != "|"){
+                arr[rows][cols] = inputs[count];
+                count++;
+            }
+        }
+    }
+}
+*/
+
+int main() { //board
+    //string arr[rows][cols];
+    vector<vector<string> > arr(rows, vector<string>(cols));
+
     int count = 1;
     for(int i = 0; i < rows; ++i) {
         for(int j = 0; j < cols; ++j) {
-            arr[i][j] = count++;
+            arr[i][j] = to_string(count);
+            count++;
         }
     }
 
+    
     // Print the array from the main function
-    cout << "Array from main function:" << endl;
+    cout << "\nArray from main function:" << endl;
+    formatting(arr);
+    getinput(arr);
+    //update(arr);
     printArray(arr);
-
+    
     return 0;
 }
 
